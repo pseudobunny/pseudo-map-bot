d045cfbe09ec0062456559423674bf050e66e876
PseudoBunny <70864101+pseudobunny@users.noreply.github.com>
2020-09-12 11:03:05 -0500

    added class, converted every other function to use it, created new structure for char_msgs.json (#2)

--- char_read/__init__.py
+++ char_read/__init__.py
@@ -1,3 +1,3 @@
-from .char_readv2 import find_trait, collect_traits, build_emb, collect_char_embs
-from .char_firstupdate import char_firstup
+from .char_collect import collect_chars, TtChar
+from .char_firstup import char_firstup
 from .char_update import char_update

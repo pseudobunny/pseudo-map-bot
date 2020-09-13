3246725e04752363837db5afc88736cadda383b7
Joel Ricker <ahsnap453@gmail.com>
2020-09-13 15:44:19 -0500

    added ignition and compression elements, along with type searching

--- item_utils/item_price.py
+++ item_utils/item_price.py
@@ -6,9 +6,16 @@ async def item_price(mess):
     try:
         item = TogItem(parameters)
 
+        if item.conflict:
+            await mess.channel.send(f"Hey, {mess.author.mention} there was an error with your flags: {item.conflict}")
+            return
+
         if item.found:
-            await mess.channel.send(f"Hey, {mess.author.mention}, the price of a {item.fetch_rank_grade()} {item.name} is {item.price} credits, and the item has {item.qp} QP.")
+            out_str = f"Hey, {mess.author.mention}, the price of a {item.fetch_rank_grade()}{item.fetch_ig_com()}{item.name} is {item.price} credits, and the item has {item.qp_mess} QP."
+            if item.fetch_dc() != 0:
+                out_str += item.fetch_dc()
+            await mess.channel.send(out_str)
         else:
             await mess.channel.send(f"Sorry {mess.author.mention}, we could not find the {item.name} item. But if we did, it would be {item.qp} QP.")
     except:
-        await mess.channel.send("Please format as \"item.price [item] [rank/grade]\"")
+        await mess.channel.send("Please format as \"item.price [flags] [item] [rank/grade]\"")
